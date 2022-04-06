c7128ac5a02c9a5c2f268f1b45fea81ebb105b15
Bruno Vidotto <brunomata51@hotmail.com.br>
2022-04-06 20:04:54 -0300

    dockerizing camio-db

--- cambio-service/src/main/java/com/vidotto/cambioservice/configuration/OpenApiConfiguration.java
+++ cambio-service/src/main/java/com/vidotto/cambioservice/configuration/OpenApiConfiguration.java
@@ -1,6 +1,7 @@
 package com.vidotto.cambioservice.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
